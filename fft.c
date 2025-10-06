4c4166dfebf0e02a28b5093c4600277c1511fa47
Saswatsit99 <86531271+Saswatsit99@users.noreply.github.com>
2025-10-06 17:54:00 +0530

    Update fft.c with optimised trigonometric functions

--- fft.c
+++ fft.c
@@ -6,6 +6,84 @@
 #define SAMPLE_RATE 16000 // adjust according to your WAV file
 #define LOW_FREQ_CUTOFF 300
 #define HIGH_FREQ_CUTOFF 10000
+
+#define TABLE_SIZE 360
+#define TWO_PI (2.0f * M_PI)
+#define HALF_PI (0.5f * M_PI)
+#define INV_TWO_PI 0.1591549430918f
+
+// --- Precomputed cosine lookup table for 0° to 359° (1° = 2π/360 radians) ---
+static const float cosine_table[TABLE_SIZE] = {
+    1.000000f, 0.999848f, 0.999391f, 0.998630f, 0.997564f, 0.996195f, 0.994522f, 0.992546f, 0.990268f, 0.987688f,
+    0.984808f, 0.981627f, 0.978148f, 0.974370f, 0.970296f, 0.965926f, 0.961262f, 0.956305f, 0.951056f, 0.945519f,
+    0.939693f, 0.933580f, 0.927184f, 0.920505f, 0.913545f, 0.906308f, 0.898794f, 0.891007f, 0.882948f, 0.874620f,
+    0.866025f, 0.857167f, 0.848048f, 0.838671f, 0.829038f, 0.819152f, 0.809017f, 0.798636f, 0.788011f, 0.777146f,
+    0.766044f, 0.754710f, 0.743145f, 0.731354f, 0.719340f, 0.707107f, 0.694658f, 0.681998f, 0.669131f, 0.656059f,
+    0.642788f, 0.629320f, 0.615661f, 0.601815f, 0.587785f, 0.573576f, 0.559193f, 0.544639f, 0.529919f, 0.515038f,
+    0.500000f, 0.484810f, 0.469472f, 0.453990f, 0.438371f, 0.422618f, 0.406737f, 0.390731f, 0.374607f, 0.358368f,
+    0.342020f, 0.325568f, 0.309017f, 0.292372f, 0.275637f, 0.258819f, 0.241922f, 0.224951f, 0.207912f, 0.190809f,
+    0.173648f, 0.156434f, 0.139173f, 0.121869f, 0.104528f, 0.087156f, 0.069756f, 0.052336f, 0.034899f, 0.017452f,
+    0.000000f, -0.017452f, -0.034899f, -0.052336f, -0.069756f, -0.087156f, -0.104528f, -0.121869f, -0.139173f, -0.156434f,
+    -0.173648f, -0.190809f, -0.207912f, -0.224951f, -0.241922f, -0.258819f, -0.275637f, -0.292372f, -0.309017f, -0.325568f,
+    -0.342020f, -0.358368f, -0.374607f, -0.390731f, -0.406737f, -0.422618f, -0.438371f, -0.453990f, -0.469472f, -0.484810f,
+    -0.500000f, -0.515038f, -0.529919f, -0.544639f, -0.559193f, -0.573576f, -0.587785f, -0.601815f, -0.615661f, -0.629320f,
+    -0.642788f, -0.656059f, -0.669131f, -0.681998f, -0.694658f, -0.707107f, -0.719340f, -0.731354f, -0.743145f, -0.754710f,
+    -0.766044f, -0.777146f, -0.788011f, -0.798636f, -0.809017f, -0.819152f, -0.829038f, -0.838671f, -0.848048f, -0.857167f,
+    -0.866025f, -0.874620f, -0.882948f, -0.891007f, -0.898794f, -0.906308f, -0.913545f, -0.920505f, -0.927184f, -0.933580f,
+    -0.939693f, -0.945519f, -0.951056f, -0.956305f, -0.961262f, -0.965926f, -0.970296f, -0.974370f, -0.978148f, -0.981627f,
+    -0.984808f, -0.987688f, -0.990268f, -0.992546f, -0.994522f, -0.996195f, -0.997564f, -0.998630f, -0.999391f, -0.999848f,
+    -1.000000f, -0.999848f, -0.999391f, -0.998630f, -0.997564f, -0.996195f, -0.994522f, -0.992546f, -0.990268f, -0.987688f,
+    -0.984808f, -0.981627f, -0.978148f, -0.974370f, -0.970296f, -0.965926f, -0.961262f, -0.956305f, -0.951056f, -0.945519f,
+    -0.939693f, -0.933580f, -0.927184f, -0.920505f, -0.913545f, -0.906308f, -0.898794f, -0.891007f, -0.882948f, -0.874620f,
+    -0.866025f, -0.857167f, -0.848048f, -0.838671f, -0.829038f, -0.819152f, -0.809017f, -0.798636f, -0.788011f, -0.777146f,
+    -0.766044f, -0.754710f, -0.743145f, -0.731354f, -0.719340f, -0.707107f, -0.694658f, -0.681998f, -0.669131f, -0.656059f,
+    -0.642788f, -0.629320f, -0.615661f, -0.601815f, -0.587785f, -0.573576f, -0.559193f, -0.544639f, -0.529919f, -0.515038f,
+    -0.500000f, -0.484810f, -0.469472f, -0.453990f, -0.438371f, -0.422618f, -0.406737f, -0.390731f, -0.374607f, -0.358368f,
+    -0.342020f, -0.325568f, -0.309017f, -0.292372f, -0.275637f, -0.258819f, -0.241922f, -0.224951f, -0.207912f, -0.190809f,
+    -0.173648f, -0.156434f, -0.139173f, -0.121869f, -0.104528f, -0.087156f, -0.069756f, -0.052336f, -0.034899f, -0.017452f
+};
+
+// --- Fast cosine approximation using lookup + interpolation ---
+float cos_a(float angle) {
+    // Wrap angle to [0, 2π)
+    angle = fabs(angle);
+    if (angle >= TWO_PI)
+        angle -= TWO_PI * (int)(angle * INV_TWO_PI);
+
+    // Convert radians to fractional index
+    float index_f = (angle * INV_TWO_PI) * TABLE_SIZE;
+    int index = (int)index_f;
+    float frac = index_f - index;
+
+    // Next index (circular)
+    int next_index = (index + 1) ;
+    if(index == TABLE_SIZE-1)
+    next_index = 0;
+
+    // Linear interpolation
+    float approx = cosine_table[index] * (1.0f - frac) + cosine_table[next_index] * frac;
+
+    return approx;
+}
+
+// --- Fast sine using cosine lookup ---
+float sin_a(float angle) {
+    // sin(θ) = cos(π/2 - θ)
+    float sign = 1.0f;
+    if(angle < 0.00) 
+    {
+        sign = -1.0f;
+        angle *= sign;
+    }
+    float shifted = HALF_PI - angle;
+
+    if (shifted < 0)
+        shifted += TWO_PI;
+    else if (shifted >= TWO_PI)
+        shifted -= TWO_PI * (int)(shifted * INV_TWO_PI);
+
+    return sign * cos_a(shifted);
+}
 // Bit-reversal permutation
 static void bit_reverse(complex *x, int N) {
     int j = 0;
@@ -37,8 +115,8 @@ void fft(float *input, complex *output, int N) {
 
     for (int s = 1; (1 << s) <= N; s++) {
         int m = 1 << s;
-        float angle = -2.0f * M_PI / m;
-        complex wm = {cosf(angle), sinf(angle)};
+        float angle = -1.0f * TWO_PI/ m;
+        complex wm = {cos_a(angle), sin_a(angle)};
         for (int k = 0; k < N; k += m) {
             complex w = {1.0f, 0.0f};
             for (int j = 0; j < m/2; j++) {
@@ -67,13 +145,13 @@ void fft(float *input, complex *output, int N) {
 void ifft(complex *in, float *out, int N) {
     for (int n = 0; n < N; n++) {
         float sum_real = 0.0f;
-        float sum_imag = 0.0f,cos_a, sin_a;
+        float sum_imag = 0.0f,cos_x, sin_x;
         for (int k = 0; k < N; k++) {
-            float angle = 2.0f * M_PI * k * n / N;
-            cos_a = cosf(angle);
-            sin_a = sinf(angle);
-            sum_real += in[k].r * cos_a - in[k].i * sin_a;
-            sum_imag += in[k].r * sin_a + in[k].i * cos_a;
+            float angle = TWO_PI * k * n / N;
+            cos_x = cos_a(angle);
+            sin_x = sin_a(angle);
+            sum_real += in[k].r * cos_x - in[k].i * sin_x;
+            sum_imag += in[k].r * sin_x + in[k].i * cos_x;
         }
         // Normalize by N
         out[n] = sum_real / N; // discard small imaginary part if input was from real FFT
@@ -110,3 +188,4 @@ void fft_process(int16_t *samples, int16_t *output)
         output[i] = (int16_t)real[i];
     }
 }
+
